495bdf2661538d0ddd0a970f916a7b92d8e0a529
Steven Roland <steven@mojave.io>
2023-05-06 12:00:06 -0500

    Add 404 page

---
+++ source/404.blade.php
@@ -0,0 +1,9 @@
+@extends('_layouts.main')
+
+@section('body')
+    <div class="flex flex-col items-center mt-32 text-gray-700">
+        <h1 class="mb-2 text-6xl font-light leading-none">404</h1>
+
+        <h2 class="text-3xl">Page not found.</h2>
+    </div>
+@endsection
