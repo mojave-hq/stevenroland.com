e50c340a661051462311c2c52552dd8f088a6cbf
Steven Roland <steven@mojave.io>
2023-06-07 20:28:15 -0500

    Update title color

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -35,7 +35,7 @@
         <link rel="icon" type="image/png" sizes="96x96" href="/assets/images/icons/favicon-96x96.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/favicon-16x16.png">
         <link rel="manifest" href="/assets/images/icons/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileColor" content="#0099cc">
         <meta name="msapplication-TileImage" content="/assets/images/icons/ms-icon-144x144.png">
         <meta name="theme-color" content="#0099cc">
 
